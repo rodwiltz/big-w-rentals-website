0425a243d929f53b06a8cddd0df061899ccfe771
rodwiltz <rodwiltz@gmail.com>
2026-08-20 14:45:36 -0500

    Add files via upload

---
+++ assets/brand/README.txt
@@ -0,0 +1,1 @@
+Brand binary integration pending. Place approved production PNG/ICO files here. Do not reconstruct artwork.
---
+++ config.js
@@ -0,0 +1,1 @@
+window.BWR_CONFIG={inquiryApiUrl:"https://script.google.com/macros/s/AKfycbxn2CsgNz2AiVwwXInQMy-ip5JSGBDPR1niacB1cRwKcuxFa3pKCcyOtkc9-liG9U8v/exec"};
