5827bcef90e73eae425b6476721bf5de64613344
rodwiltz <rodwiltz@gmail.com>
2026-08-27 15:46:09 -0500

    EWO-BWR-002 — Guided Rental Funnel & Lead Notification

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.BWR_CONFIG={inquiryApiUrl:"https://script.google.com/macros/s/AKfycbxn2CsgNz2AiVwwXInQMy-ip5JSGBDPR1niacB1cRwKcuxFa3pKCcyOtkc9-liG9U8v/exec"};
+window.BWR_CONFIG=Object.freeze({leadApiUrl:"https://script.google.com/macros/s/AKfycbxn2CsgNz2AiVwwXInQMy-ip5JSGBDPR1niacB1cRwKcuxFa3pKCcyOtkc9-liG9U8v/exec"});
